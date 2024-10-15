4e8a9b04670e4fcba9fb39fd884c2a6e3bfb9ad9
Bruno Padilha <brpadilha.dev@gmail.com>
2024-10-15 14:24:58 -0300

    Add form for creating entries

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,34 +5,84 @@ export default function Index() {
       <p className="mt-3 text-xl text-gray-400">
         Doing and learnings. Update weekly.
       </p>
+      <div className="my-8 border p-2">
+        <form>
+          <p className="italic">Create an entry</p>
+          <div>
+            <div className="mt-4">
+              <input type="date" name="date" className="text-gray-700" />
+            </div>
+            <div className="space-x-6 mt-2">
+              <label>
+                <input
+                  className="mr-2"
+                  type="radio"
+                  value="work"
+                  name="category"
+                />
+                Work
+              </label>
+              <label>
+                <input
+                  className="mr-2"
+                  type="radio"
+                  value="learning"
+                  name="category"
+                />
+                Learning
+              </label>
+              <label>
+                <input
+                  className="mr-2"
+                  type="radio"
+                  value="interesting-things"
+                  name="category"
+                />
+                Interesting things
+              </label>
+            </div>
 
-      <div className="mt-4">
-          <p
-            className="font-bold"
-          >
-            Week of Feb 20<sup>th</sup>, 2023
-          </p>
-            <div className="mt-3 space-y-4">
-              <div>
-                <p>Work:</p>
-                <ul className="ml-6 list-disc">
-                  <li>First item</li>
-                </ul>
-              </div>
-              <div>
-                <p>Learnings: </p>
-                <ul className="ml-6 list-disc">
-                  <li>First learning</li>
-                  <li>Second learning</li>
-                </ul>
-              </div>
-              <div>
-                <p>Interesting things:</p>
-                <ul className="ml-6 list-disc">
-                  <li>Something cool!</li>
-                </ul>
+            <div className="mt-2">
+              <textarea
+                name="entry"
+                placeholder="Write your entry..."
+                className="w-full text-gray-700"
+              />
+              <div className="mt-2 text-right">
+                <button className="bg-blue-500 py-1 px-4 font-medium text-white" type="submit">
+                  Save
+                </button>
               </div>
             </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="mt-4">
+        <p className="font-bold">
+          Week of Feb 20<sup>th</sup>, 2023
+        </p>
+        <div className="mt-3 space-y-4">
+          <div>
+            <p>Work:</p>
+            <ul className="ml-6 list-disc">
+              <li>First item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Learnings: </p>
+            <ul className="ml-6 list-disc">
+              <li>First learning</li>
+              <li>Second learning</li>
+            </ul>
+          </div>
+          <div>
+            <p>Interesting things:</p>
+            <ul className="ml-6 list-disc">
+              <li>Something cool!</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );

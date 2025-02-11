9aaa23cec830d010612d922a113ed350a9dc899d
Bruno Padilha <brpadilha.dev@gmail.com>
2025-02-11 12:33:00 -0300

    feat: add pending status and disable form when status is submitting

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { ActionFunctionArgs } from "@remix-run/node";
-import { Form, redirect } from "@remix-run/react";
+import { redirect, useFetcher } from "@remix-run/react";
 
 export async function action({ request }: ActionFunctionArgs) {
+  // simulate long load from api
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const db = new PrismaClient();
 
   const formData = await request.formData();
@@ -32,6 +34,8 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function Index() {
+  const fetcher = useFetcher()
+  console.log(fetcher.state)
   return (
     <div className="p-10">
       <h1 className="text-5xl">Work journal</h1>
@@ -39,15 +43,29 @@ export default function Index() {
         Doing and learnings. Update weekly.
       </p>
       <div className="my-8 border p-2">
-        <Form method="post">
+        <fieldset 
+          className="disabled:opacity-70"
+          disabled={fetcher.state === "submitting"}>
+        <fetcher.Form method="post" className="mt-2">
           <p className="italic">Create an entry</p>
           <div>
             <div className="mt-4">
-              <input type="date" name="date" className="text-gray-700" />
+              <input
+                type="date"
+                name="date"
+                className="text-gray-700"
+                required
+              />
             </div>
             <div className="space-x-6 mt-2">
               <label>
-                <input className="mr-2" type="radio" value="work" name="type" />
+                <input
+                  className="mr-2"
+                  type="radio"
+                  value="work"
+                  name="type"
+                  required
+                />
                 Work
               </label>
               <label>
@@ -56,6 +74,7 @@ export default function Index() {
                   type="radio"
                   value="learning"
                   name="type"
+                  required
                 />
                 Learning
               </label>
@@ -65,6 +84,7 @@ export default function Index() {
                   type="radio"
                   value="interesting-things"
                   name="type"
+                  required
                 />
                 Interesting things
               </label>
@@ -75,18 +95,21 @@ export default function Index() {
                 name="text"
                 placeholder="Write your entry..."
                 className="w-full text-gray-700"
+                required
               />
               <div className="mt-2 text-right">
                 <button
                   className="bg-blue-500 py-1 px-4 font-medium text-white"
                   type="submit"
                 >
-                  Save
+                  {fetcher.state === "submitting" ? "Saving...": "Save"}
                 </button>
               </div>
             </div>
           </div>
-        </Form>
+        </fetcher.Form>
+        </fieldset>
+       
       </div>
 
       <div className="mt-4">

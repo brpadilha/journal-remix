f5adc6b6e477f4bb66db167e549c6ca8ee9764a4
Bruno Padilha <brpadilha.dev@gmail.com>
2025-02-11 17:40:23 -0300

    feat: apply loading

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { ActionFunctionArgs } from "@remix-run/node";
-import { redirect, useFetcher } from "@remix-run/react";
+import { useFetcher } from "@remix-run/react";
+import { format } from "date-fns";
+import { useEffect, useRef } from "react";
 
 export async function action({ request }: ActionFunctionArgs) {
-  // simulate long load from api
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   const db = new PrismaClient();
 
   const formData = await request.formData();
-  const { date, type, text } = Object.fromEntries(formData.entries()) as {
-    date: string;
-    type: string;
-    text: string;
-  };
+  const values = Object.fromEntries(formData);
+  const { date, type, text } = values;
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   if (
     typeof date !== "string" ||
@@ -22,20 +21,28 @@ export async function action({ request }: ActionFunctionArgs) {
     throw new Error("Bad request");
   }
 
-  await db.entry.create({
+  return db.entry.create({
     data: {
       date: new Date(date),
       type: type,
       text: text,
     },
   });
-
-  return redirect("/");
 }
 
+
 export default function Index() {
-  const fetcher = useFetcher()
-  console.log(fetcher.state)
+  const fetcher = useFetcher();
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  
+
+useEffect(() => {
+  if(fetcher.state === "idle" && textAreaRef.current) {
+    textAreaRef.current.value = "";
+    textAreaRef.current.focus() 
+  }
+}, [fetcher.state]);
+
   return (
     <div className="p-10">
       <h1 className="text-5xl">Work journal</h1>
@@ -43,73 +50,76 @@ export default function Index() {
         Doing and learnings. Update weekly.
       </p>
       <div className="my-8 border p-2">
-        <fieldset 
+        <fieldset
           className="disabled:opacity-70"
-          disabled={fetcher.state === "submitting"}>
-        <fetcher.Form method="post" className="mt-2">
-          <p className="italic">Create an entry</p>
-          <div>
-            <div className="mt-4">
-              <input
-                type="date"
-                name="date"
-                className="text-gray-700"
-                required
-              />
-            </div>
-            <div className="space-x-6 mt-2">
-              <label>
+          disabled={fetcher.state === "submitting"}
+        >
+          <fetcher.Form method="post" className="mt-2">
+            <p className="italic">Create an entry</p>
+            <div>
+              <div className="mt-4">
                 <input
-                  className="mr-2"
-                  type="radio"
-                  value="work"
-                  name="type"
+                  type="date"
+                  name="date"
+                  className="text-gray-900"
+                  defaultValue={format(new Date(), "yyyy-MM-dd")}
                   required
                 />
-                Work
-              </label>
-              <label>
-                <input
-                  className="mr-2"
-                  type="radio"
-                  value="learning"
-                  name="type"
-                  required
-                />
-                Learning
-              </label>
-              <label>
-                <input
-                  className="mr-2"
-                  type="radio"
-                  value="interesting-things"
-                  name="type"
-                  required
-                />
-                Interesting things
-              </label>
-            </div>
+              </div>
+              <div className="space-x-6 mt-2">
+                <label>
+                  <input
+                    className="mr-2"
+                    defaultChecked
+                    type="radio"
+                    value="work"
+                    name="type"
+                    required
+                  />
+                  Work
+                </label>
+                <label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    value="learning"
+                    name="type"
+                    required
+                  />
+                  Learning
+                </label>
+                <label>
+                  <input
+                    className="mr-2"
+                    type="radio"
+                    value="interesting-things"
+                    name="type"
+                    required
+                  />
+                  Interesting things
+                </label>
+              </div>
 
-            <div className="mt-2">
-              <textarea
-                name="text"
-                placeholder="Write your entry..."
-                className="w-full text-gray-700"
-                required
-              />
-              <div className="mt-2 text-right">
-                <button
-                  className="bg-blue-500 py-1 px-4 font-medium text-white"
-                  type="submit"
-                >
-                  {fetcher.state === "submitting" ? "Saving...": "Save"}
-                </button>
+              <div className="mt-2">
+                <textarea
+                  name="text"
+                  placeholder="Write your entry..."
+                  className="w-full text-gray-700"
+                  ref={textAreaRef}
+                  required
+                />
+                <div className="mt-2 text-right">
+                  <button
+                    className="bg-blue-500 py-1 px-4 font-medium text-white"
+                    type="submit"
+                  >
+                    {fetcher.state === "submitting" ? "Saving..." : "Save"}
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </fetcher.Form>
+          </fetcher.Form>
         </fieldset>
-       
       </div>
 
       <div className="mt-4">
